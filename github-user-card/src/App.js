e1b1597461334bcb1120dda557bed26cc606eaf3
aydinsenih <aydinsenih@gmail.com>
2021-01-28 10:32:41 -0800

    style changes

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -5,81 +5,87 @@ import User from "./components/User";
 import Followers from "./components/Followers";
 const { Header: AntHeader } = Layout;
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      userData: {},
-      followers: [],
-      username: "",
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            userData: {},
+            followers: [],
+            username: "",
+        };
+    }
 
-  getUser = (values) => {
-    console.log(this.state);
-    this.setState({
-      ...this.state,
-      username: values.username,
-    });
-    axios
-      .get("https://api.github.com/users/" + values.username)
-      .then((res) => {
+    getUser = (values) => {
+        console.log(this.state);
         this.setState({
-          ...this.state,
-          userData: res.data,
-        });
-        axios.get(res.data.followers_url).then((res) => {
-          this.setState({
             ...this.state,
-            followers: res.data,
-          });
+            username: values.username,
         });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+        axios
+            .get("https://api.github.com/users/" + values.username)
+            .then((res) => {
+                this.setState({
+                    ...this.state,
+                    userData: res.data,
+                });
+                axios.get(res.data.followers_url).then((res) => {
+                    this.setState({
+                        ...this.state,
+                        followers: res.data,
+                    });
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
 
-  render() {
-    return (
-      <Layout>
-        <AntHeader className="header">
-          <div>Github User Card</div>
-        </AntHeader>
-        <Form className="forms" name="search" onFinish={this.getUser}>
-          <Form.Item
-            name="username"
-            rules={[
-              {
-                required: true,
-                message: "please input your username!",
-              },
-            ]}
-          >
-            <Input name="username" placeholder="username" />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Search
-            </Button>
-          </Form.Item>
-        </Form>
-        {console.log(this.state.userData)}
-        {this.state.userData.name ? (
-          <div className="user-info">
-            <User data={this.state.userData} />
-            <h1>{this.state.userData.name} Followers</h1>
-            <div className="user-followers">
-              {this.state.followers.map((user) => {
-                return <Followers data={user} />;
-              })}
-            </div>
-          </div>
-        ) : (
-          <div></div>
-        )}
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <AntHeader className="header">
+                    <div>Github User Card</div>
+                </AntHeader>
+                <Form
+                    className="forms"
+                    name="search"
+                    layout="vertical"
+                    onFinish={this.getUser}
+                >
+                    <Form.Item
+                        label="Github Username:"
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: "please input your username!",
+                            },
+                        ]}
+                    >
+                        <Input name="username" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">
+                            Search
+                        </Button>
+                    </Form.Item>
+                </Form>
+                {console.log(this.state.userData)}
+                {this.state.userData.name ? (
+                    <div className="user-info">
+                        <User data={this.state.userData} />
+                        <h1>{this.state.userData.name} Followers</h1>
+                        <div className="user-followers">
+                            {this.state.followers.map((user) => {
+                                return <Followers data={user} />;
+                            })}
+                        </div>
+                    </div>
+                ) : (
+                    <div></div>
+                )}
+            </Layout>
+        );
+    }
 }
 
 export default App;
